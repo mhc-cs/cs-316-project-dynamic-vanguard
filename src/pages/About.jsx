9f1af8e49adc37c7bf0a6394cead0eab6c1671b5
Nikki <berns23n@mtholyoke.edu>
2024-02-26 14:35:40 -0500

    pushing demos to demo branch

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <p>About.jsx</p>
+    )
+}
+
+export default About
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <p>Contact.jsx</p>
+    )
+}
+
+export default Contact
