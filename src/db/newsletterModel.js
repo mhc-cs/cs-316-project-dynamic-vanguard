ba2962dc96fb2060fddd152a415e99c0178aeff0
Fevronia Van Sickle <vansi22f@mtholyoke.edu>
2024-04-16 12:03:47 -0400

    tinkered around with models

--- src/db/newsletterModel.js
+++ src/db/newsletterModel.js
@@ -21,6 +21,7 @@ const transporter = nodemailer.createTransport({
 const newsletterSchema = new mongoose.Schema({
     email: String,
     name: String,
+    message: String,
     subscribed: {
         type: Boolean,
         default: true
@@ -47,7 +48,7 @@ export async function addToNewsletter(email) {
     const mailOptions = {
         from: 'vansi22f@mtholyoke.edu',
         to: email,
-        subject: 'Welcome!', //no name in order to reuse code
+        subject: 'Welcome!', 
         text: 'Welcome Warrior!\nThank you so much for becoming a part of our community!\nTo never miss our monthly newsletter, make sure to:\nSave this email address to your contacts so it doesn\'t accidentally go to spam.\nCheers,\nAya Wild\nYou received this email because you subscribed to our list. You can unsubscribe at any time.'
     };
 
@@ -58,16 +59,36 @@ export async function addToNewsletter(email) {
  * sends email for free session 
  *  @param {*} email 
  */
-export async function scheduleSession(email) {
+export async function scheduleSession(email, name, message) {
+   
+    //send scheduling email to client
     const mailOptions = {
         from: 'vansi22f@mtholyoke.edu',
         to: email,
-        subject: 'Welcome!', //no name in order to reuse code
-        text: 'Welcome Warrior! Schedule now'
+        subject: 'Welcome'+ name + '!',
+        text: 'Welcome Warrior! Schedule your free session'
     };
-
     await transporter.sendMail(mailOptions);
+    await scheduleSession(email, name, message);
 }
+
+/**
+ * notify Aya of request for free session 
+ *  @param {*} email 
+ */
+export async function scheduleSession(email, name, message) {
+   
+    //send email to Aya with client request info
+    const mailOptions = {
+        from: 'vansi22f@mtholyoke.edu',
+        to: 'yabra22n@mtholyoke.edu',
+        subject: name + 'has requested a free session!',
+        text: 'Hi Aya,\n' + name + 'has been sent an email to schedule their free session with you.\nTheir message:\n'+message
+    };
+    await transporter.sendMail(mailOptions);
+
+}
+
 /**
  * get all subscribers in collection 
  * @returns subscribers
